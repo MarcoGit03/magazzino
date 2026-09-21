b3ff1d624cdd9b396550eb942043500026f2195f
MarcoLori <madonnaquantiaccountcho@gmail.com>
2026-09-21 14:18:33 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configurazione del progetto Firebase "magazzino".
+// Questi valori NON sono password: e' normale che siano visibili nel sito.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAaky4btEfzgGmbbxbxOgFZJhoTOz0y83c",
+  authDomain: "magazzino-3ddf9.firebaseapp.com",
+  projectId: "magazzino-3ddf9",
+  storageBucket: "magazzino-3ddf9.firebasestorage.app",
+  messagingSenderId: "106996035089",
+  appId: "1:106996035089:web:5c36231d2853b6cd7c8bb0"
+};
